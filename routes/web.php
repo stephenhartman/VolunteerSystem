90dc3ef878c32976d1e37ee73e3b988da24b5c97
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-07 17:06:34 -0400

    Switch up the view and parameter

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,16 @@
 */
 
 Route::get('/', function () {
-    $people = ['Steve', 'Kevin', 'Lindsey', 'Andrew'];
 
-    return view('welcome', compact('people'));
+    return view('welcome');
 });
 
 Route::get('about', function() {
-    return view('about');
+    $people = ['Steve Hartman', 'Kevin Bell', 'Lindsey Wanta', 'Andrew Greer'];
+
+    return view('pages.about', compact('people')); // resources/views/pages/about.blade.php
+    //return view('pages.about', ['people' => $people]); pass array
+    //return view('pages.about', compact('people'); pass array created with php
+    //return view('pages.about')->with('people', $people); use with method
+    //return view('pages.about')->withPeople($people); dynamic method
 });
