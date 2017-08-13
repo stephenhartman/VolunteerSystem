4f55a01dcdfd6d5c4b17d2ef5cbf59a126a82ac0
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-13 18:23:08 -0400

    Modify views

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -4,4 +4,9 @@
 
     <h1>All Volunteers</h1>
 
+    @foreach ($members as $member)
+        <div>
+            <a href="{{ $member->path() }}">{{ $member->first_name }} {{ $member->last_name }}</a>
+        </div>
+    @endforeach
 @stop
