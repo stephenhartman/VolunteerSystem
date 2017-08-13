d8637bbefd778dd1f7e34f1bdc110350c62fed48
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-13 18:22:57 -0400

    Add factories and seed data

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Member::class, function (Faker\Generator $faker) {
+
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'username' => $faker->userName,
+        'password' => bcrypt('secret'),
+        'street' => $faker->streetAddress,
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'zipcode' => $faker->postcode,
+        'home_phone' => $faker->phoneNumber,
+        'work_phone' => $faker->phoneNumber,
+        'cell_phone' => $faker->phoneNumber,
+        'drivers_license' => $faker->boolean($chanceOfGettingTrue = 50),
+        'ss_card' => $faker->boolean($chanceOfGettingTrue = 50),
+        'approval_status' => $faker->randomElement($array = array ('pending', 'approved', 'rejected')),
+        'user_id' => function() {
+            return factory(App\User::class)->create()->id;
+        },
+        'emergency_contact_id' => function() {
+            return factory(App\EmergencyContact::class)->create()->id;
+        }
+    ];
+});
