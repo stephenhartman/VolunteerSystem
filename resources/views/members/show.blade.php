aef93c165e56fafc8ea2aa7e1893846e58a3d88f
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-13 18:23:08 -0400

    Modify views

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
     <h1>{{ $member->first_name }} {{ $member->last_name }}</h1>
+    <ul>
+        @foreach ($member->emergency_contacts as $contact)
+            <li>{{ $contact }}</li>
+        @endforeach
+    </ul>
 @stop
