dc11c1784448b93f84a542275744ea2463fa6c91
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-13 18:22:33 -0400

    Modify models

--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'password', 'street', 'city', 'state', 'zipcode', 'home_phone', 'work_phone', 'cell_phone', 'drivers_license', 'ss_card', 'approval_status',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public function emergency_contacts()
+    {
+        return $this->hasMany(EmergencyContact::class);
+    }
+
+    public function path()
+    {
+        return '/members/' . $this->id;
+    }
 }
