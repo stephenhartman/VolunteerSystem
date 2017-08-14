127591395084e8046d7e2cb19a3132c87591f5f7
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-14 14:17:36 -0400

    Tweak migrations and models

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <h1>{{ $member->first_name }} {{ $member->last_name }}</h1>
     <ul>
+        Emergency Contacts
         @foreach ($member->emergency_contacts() as $contact)
-            <li>{{ $contact }}</li>
+            <li>{{ $contact->first_name }} {{ $contact->last_name }}</li>
         @endforeach
     </ul>
 @stop
