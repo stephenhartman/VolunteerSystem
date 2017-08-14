2fd3845740cd58436c285d1b8d7ff9c1076e1988
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-13 22:56:07 -0400

    WIP

--- database/migrations/2017_08_13_163641_create_members_table.php
+++ database/migrations/2017_08_13_163641_create_members_table.php
@@ -15,8 +15,6 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->integer('emergency_contact_id')->unsigned();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
@@ -33,11 +31,6 @@ class CreateMembersTable extends Migration
             $table->boolean('ss_card');
             $table->string('approval_status');
             $table->timestamps();
-        });
-
-        Schema::table('members', function($table) {
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('emergency_contact_id')->references('id')->on('emergency_contacts')->onDelete('cascade');
         });
     }
 
