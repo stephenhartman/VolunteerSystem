395c6c0016f771df955cdf59f754280c9e911f20
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-15 20:51:42 -0400

    Modify CRUD views WIP

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -4,9 +4,12 @@
 
     <h1>All Volunteers</h1>
 
-    @foreach ($members as $member)
-        <div>
-            <a href="{{ $member->path() }}">{{ $member->first_name }} {{ $member->last_name }}</a>
-        </div>
+    @if (Session::has('message'))
+        <div class="alert alert-info">{{ Session::get('message') }}</div>
+    @endif
+        @foreach ($members as $member)
+            <div>
+                <a href="{{ $member->path() }}">{{ $member->first_name }} {{ $member->last_name }}</a>
+            </div>
     @endforeach
 @stop
