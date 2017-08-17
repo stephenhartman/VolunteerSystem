79488fa41cfbb7be1bb79474acfeb9bad95e0419
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-17 16:49:15 -0400

    Create controllers

---
+++ app/Http/Controllers/OpportunitiesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Opportunity;
+use Illuminate\Http\Request;
+
+class OpportunitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $opportunities = Opportunity::all();
+        return view('opportunities.index', compact('opportunities'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('opportunities.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Opportunity  $opportunity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Opportunity $opportunity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Opportunity  $opportunity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Opportunity $opportunity)
+    {
+        //
+    }
+}
