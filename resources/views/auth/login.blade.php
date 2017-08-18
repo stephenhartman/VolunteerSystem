fcf7a1810fc94e450b2ba6ab279003d067cf6ab3
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-18 16:17:50 -0400

    Integrate home view and add auth to navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+    @if (Session::has('message'))
+        <div class="alert alert-info">{{ Session::get('message') }}</div>
+    @endif
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -8,12 +8,12 @@
   </p>
 @endif
 
-@if (Auth::guard('admin')->check())
-  <p class="text-success">
-    You are Logged In as a <strong>ADMIN</strong>
-  </p>
-@else
-  <p class="text-danger">
-    You are Logged Out as a <strong>ADMIN</strong>
-  </p>
-@endif
+{{-- @if (Auth::guard('admin')->check()) --}}
+{{--   <p class="text-success"> --}}
+{{--     You are Logged In as a <strong>ADMIN</strong> --}}
+{{--   </p> --}}
+{{-- @else --}}
+{{--   <p class="text-danger"> --}}
+{{--     You are Logged Out as a <strong>ADMIN</strong> --}}
+{{--   </p> --}}
+{{-- @endif --}}
