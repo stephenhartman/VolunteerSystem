da8425749a33a23a7e29fd72715c2376bd8602af
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-18 15:47:14 -0400

    View tweaks

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">ADMIN Dashboard</div>
-
                 <div class="panel-body">
                     @component('components.who')
                     @endcomponent
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,14 +13,10 @@
         @endif
 
         <div class="content">
-		<img src="{{url('/images/volunteer.jpg')}}" alt="Image"/>
+            <img src="{{url('/images/volunteer.jpg')}}" alt="Image"/>
             <div class="title m-b-md">
-                
-				
-				
-				Laravel Volunteer System
+                Laravel Volunteer System
             </div>
-
             <div class="links">
                 <a href="about">About Us</a>
                 <a href="https://laravel.com/docs">Documentation</a>
