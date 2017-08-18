4d0e99b577c626a066621b84181821c22f1faaaa
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-17 21:12:29 -0400

    Update the factories

--- database/factories/InterestsFactory.php
+++ database/factories/InterestsFactory.php
@@ -5,5 +5,5 @@ $factory->define(App\Interests::class, function (Faker\Generator $faker) {
 
     return [
         'interest' => $faker->safeColorName,
-        'member_id' => rand(1,10)];
+        'member_id' => rand(1,25)];
 });
