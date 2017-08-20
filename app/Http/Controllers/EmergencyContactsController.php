65fbba20bc258a29b29351d904aae234b931e28f
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-20 09:16:19 -0400

    Rework contact controller

--- app/Http/Controllers/EmergencyContactsController.php
+++ app/Http/Controllers/EmergencyContactsController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\EmergencyContact;
+use App\Member;
 use Illuminate\Http\Request;
 
-class EmergencyContact extends Controller
+class EmergencyContactsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Member $member)
     {
         $contacts = EmergencyContact::all();
         return view('emergency_contacts.index', compact('contacts'));
@@ -21,9 +23,10 @@ class EmergencyContact extends Controller
     /**
      * Show the form for creating a new resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Member $member)
     {
         return view('emergency_contacts.create');
     }
@@ -32,9 +35,10 @@ class EmergencyContact extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Member $member)
     {
         //
     }
@@ -42,10 +46,11 @@ class EmergencyContact extends Controller
     /**
      * Display the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\EmergencyContact  $emergencyContact
      * @return \Illuminate\Http\Response
      */
-    public function show(EmergencyContact $emergencyContact)
+    public function show(Member $member, EmergencyContact $emergencyContact)
     {
         //
     }
@@ -53,10 +58,11 @@ class EmergencyContact extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\EmergencyContact  $emergencyContact
      * @return \Illuminate\Http\Response
      */
-    public function edit(EmergencyContact $emergencyContact)
+    public function edit(Member $member, EmergencyContact $emergencyContact)
     {
         //
     }
@@ -65,10 +71,11 @@ class EmergencyContact extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @param  \App\EmergencyContact  $emergencyContact
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, EmergencyContact $emergencyContact)
+    public function update(Request $request, Member $member, EmergencyContact $emergencyContact)
     {
         //
     }
@@ -76,10 +83,11 @@ class EmergencyContact extends Controller
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Member  $member
      * @param  \App\EmergencyContact  $emergencyContact
      * @return \Illuminate\Http\Response
      */
-    public function destroy(EmergencyContact $emergencyContact)
+    public function destroy(Member $member, EmergencyContact $emergencyContact)
     {
         //
     }
--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Member;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 class MemberController extends Controller
 {
