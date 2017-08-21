4d8d6000dbced6ee472c224ffdff5cf776afcf0e
stephenhartman <stephen.c.hartman@gmail.com>
2017-08-21 09:46:03 -0400

    Simple controllers

--- app/Http/Controllers/InterestsController.php
+++ app/Http/Controllers/InterestsController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Interests;
+use App\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
 
 class InterestsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Member $member)
     {
         //
     }
@@ -20,31 +25,47 @@ class InterestsController extends Controller
     /**
      * Show the form for creating a new resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Member $member)
     {
-        //
+        return view('members.interests.create', compact('member'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Member $member, Interests $interest)
     {
-        //
+        $this->validate($request, array(
+            'interest' => 'required',
+        ));
+
+        //store
+
+        $interest->interest = $request->interest;
+        $interest->member_id = $member->id;
+
+        $interest->save();
+
+        Session::flash('success', 'The interest was successfully created!');
+
+        return redirect()->route('members.show', compact('member'));
     }
 
     /**
      * Display the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\Interests  $interests
      * @return \Illuminate\Http\Response
      */
-    public function show(Interests $interests)
+    public function show(Member $member, Interests $interests)
     {
         //
     }
@@ -52,10 +73,11 @@ class InterestsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\Interests  $interests
      * @return \Illuminate\Http\Response
      */
-    public function edit(Interests $interests)
+    public function edit(Member $member, Interests $interests)
     {
         //
     }
@@ -64,10 +86,11 @@ class InterestsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @param  \App\Interests  $interests
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Interests $interests)
+    public function update(Request $request, Member $member, Interests $interests)
     {
         //
     }
@@ -75,10 +98,11 @@ class InterestsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Member  $member
      * @param  \App\Interests  $interests
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Interests $interests)
+    public function destroy(Member $member, Interests $interests)
     {
         //
     }
--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -3,48 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Skill;
+use App\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
 
 class SkillsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Member $member)
     {
-        //
     }
 
     /**
      * Show the form for creating a new resource.
      *
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Member $member)
     {
-        //
+        return view('members.skills.create', compact('member'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Member $member, Skill $skill)
     {
-        //
+        $this->validate($request, array(
+            'skill' => 'required',
+
+        ));
+
+        //store
+
+        $skill->skill = $request->skill;
+        $skill->skill_level = $request->skill_level;
+        $skill->member_id = $member->id;
+
+        if ($skill->skill_level == null)
+        {
+            $skill->skill_level = 0;
+        }
+        $skill->save();
+
+        Session::flash('success', 'The skill was successfully created!');
+
+        return redirect()->route('members.show', compact('member'));
     }
 
     /**
      * Display the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\Skill  $skill
      * @return \Illuminate\Http\Response
      */
-    public function show(Skill $skill)
+    public function show(Member $member, Skill $skill)
     {
         //
     }
@@ -52,10 +78,11 @@ class SkillsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Member  $member
      * @param  \App\Skill  $skill
      * @return \Illuminate\Http\Response
      */
-    public function edit(Skill $skill)
+    public function edit(Member $member, Skill $skill)
     {
         //
     }
@@ -64,10 +91,11 @@ class SkillsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Member  $member
      * @param  \App\Skill  $skill
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Skill $skill)
+    public function update(Request $request, Member $member, Skill $skill)
     {
         //
     }
@@ -75,10 +103,11 @@ class SkillsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Member  $member
      * @param  \App\Skill  $skill
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Skill $skill)
+    public function destroy(Member $member, Skill $skill)
     {
         //
     }
