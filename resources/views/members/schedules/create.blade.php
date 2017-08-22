479d205ba727db758ba01496e67a318eab907e7c
jawsofdoom <kbell140@gmail.com>
2017-08-21 22:33:16 -0400

    added schedules to members.show added schedules controller added create and edit views to schedules controller

---
+++ resources/views/members/schedules/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            {{ Form::open(array('route' => array('members.schedules.store', $member))) }}
+            {{Form::label('start_time', 'Start Time')}}
+            {{Form::time('start_time', null, array('class'=> 'form-control'))}}
+
+            {{Form::label('end_time', 'End Time')}}
+            {{Form::time('end_time', null, array('class'=> 'form-control'))}}
+
+            {{Form::label('day_id', 'Day')}}
+            {{Form::select('day_id', [ 1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday', 4 => 'Thursday', 5 => 'Friday', 6 => 'Saturday', 7 => 'Sunday'], array('class'=> 'form-control'))}}
+
+            {{Form::label('notes', 'Notes')}}
+            {{Form::text('notes', null, array('class'=> 'form-control'))}}
+
+
+            <br>
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Html::linkRoute('members.show', 'Cancel', array($member), array('class' => "btn btn-danger btn-block")) !!}
+                </div>
+                <div class="col-sm-6">
+                    {{ Form::submit('Save Changes', ['class' => "btn btn-success btn-block"]) }}
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
